49be1735574cd46bc6b29c648c01d916a1674845
Josh <j-et@live.co.uk>
2023-11-08 21:53:26 +0000

    added the build step with protobufs

--- boilerplate/src/main.rs
+++ boilerplate/src/main.rs
@@ -1,15 +1,6 @@
-use std::{error::Error, fs::read_dir};
+use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // for all files in proto, run tonic_build::compile_proto
-
-    let dir = read_dir("proto")?;
-
-    dir.map_err(|f| {
-        let p = f.expect("Failed to read file").path();
-        tonic_build::compile_protos(p)?;
-    });
-
     Ok(())
 }
