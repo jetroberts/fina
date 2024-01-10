e71001933d38f6ab110a113b2d5b1e540c9f9a3b
Josh Roberts <j-et@live.co.uk>
2024-01-10 23:32:15 +0000

    feat: added shared state between service and webserver

--- import/src/server.rs
+++ import/src/server.rs
@@ -1,32 +1,39 @@
+use std::{
+    io::BufRead,
+    sync::{Arc, Mutex, RwLock},
+};
+
 use axum::{
     extract::Multipart,
     http::StatusCode,
     routing::{get, post},
-    Router,
+    Extension, Router,
 };
 
 use crate::service::service::Service;
 
 pub struct Server {
-    parse_server: Service,
+    parse_server: Arc<RwLock<Service>>,
 }
 
 impl Server {
     pub fn new() -> Self {
         Self {
-            parse_server: Service::new(),
+            parse_server: Arc::new(RwLock::new(Service::new())),
         }
     }
 
     pub async fn start(&self) -> Result<(), String> {
         let app = Router::new()
             .route("/", get("Ok"))
-            .route("/upload", post(upload));
+            .route("/upload", post(upload))
+            .layer(Extension(self.parse_server.clone()));
 
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
             .await
             .expect("enable to bind to port 3000");
 
+        println!("Listening on port 3000");
         axum::serve(listener, app)
             .await
             .expect("failed to run server");
@@ -35,6 +42,27 @@ impl Server {
     }
 }
 
-async fn upload(multipart: Multipart) -> Result<StatusCode, ()> {
+async fn upload(
+    Extension(parse_service): Extension<Arc<Mutex<Service>>>,
+    mut multipart: Multipart,
+) -> Result<StatusCode, ()> {
+    let mut count = 0;
+
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .expect("failed to read multipart")
+    {
+        if let Some(filename) = field.file_name() {
+            println!("filename: {}", filename)
+        }
+
+        if let Ok(bytes) = field.bytes().await {
+            if let Ok(ps) = parse_service.lock() {
+                ps.parse(bytes.as_ref());
+            }
+        }
+    }
+
     Ok(StatusCode::OK)
 }
